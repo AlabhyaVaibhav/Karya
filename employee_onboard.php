3d1a2107760d17397d3cd80c3f9ba856887824b2
AlabhyaVaibhav <alabhya.vaibhav1997@gmail.com>
2018-05-05 18:30:28 +0530

    Rename the files

---
+++ employee_onboard.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+include('connect.php');
+		function getaadharTrim($message)
+		{
+			$first12 = substr($message, 0,12);
+			return $first12;	
+		}
+		function getmobileTrim($message)
+		{
+			$last = substr($message,12,21);
+			return $last;
+		}
+	  if( $_GET["message"]) {
+      	echo "Recevied". " ".$_GET['message']. "<br />";
+      	$message =$_GET['message'];
+      	$aadhar_number = getaadharTrim($message);
+      	$mobile_number = getmobileTrim($message);
+      	$sql = "INSERT sms_string SET string = :message,
+      	mobile = :mobile_number,
+      	aadhar = :aadhar_number";
+      	$stmt = $con->prepare($sql);
+      	$stmt->bindParam(':message', $_GET['message'],PDO::PARAM_STR);
+      	$stmt->bindParam(':aadhar_number', $aadhar_number,PDO::PARAM_STR);
+      	$stmt->bindParam(':mobile_number', $mobile_number,PDO::PARAM_STR);      
+   		$stmt->execute();
+   		echo 'Sent succesfully!'.$message;
+
+   }
+?>
