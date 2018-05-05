e554b49966bec30d581c6b246b713b63406545ab
Akshit Pradhan <akshitnnl@gmail.com>
2018-05-05 23:48:56 +0530

    Add files via upload

---
+++ dict.py
@@ -0,0 +1,8 @@
+
+# coding: utf-8
+
+# In[5]:
+
+
+skills = ['plough','farm','farming','ploughing','painting','painter','harvest','harvesting','manufacture','manufacturing','janitor','mine','mining','construct','construction','mechanic','builder','building','maintain','maintenance','warehouse','warehousing','laundry','plumber','electrician','aircraft','mechannic','electrician','machinist','pipefitter']
+
